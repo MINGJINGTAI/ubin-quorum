40a8790f4626e7fb1388d62fb39fbd530f4f9113
seblkma <sebmalikkeung@gmail.com>
2018-06-25 11:15:10 +0800

    Added comment with url to web3 httpprovider.js

--- solc-local/deploy_multi.js
+++ solc-local/deploy_multi.js
@@ -2,14 +2,15 @@ const fs = require('fs');
 const solc = require('solc');
 const Web3 = require('web3');
 
-// Seb : host and port to read from deployment script
+// Seb: host and port to read from deployment script, otherwise default to Project Ubin nodes
 var host = process.argv[2];
 var port = process.argv[3];
 host = (host != null && host.length > 0) ? host : "quorumnx01.southeastasia.cloudapp.azure.com";
 port = (port != null && port.length > 0) ? port : "20010";
 
 const web3 = new Web3(new Web3.providers.HttpProvider("http://" + host + ":" + port));
-// Seb : check Web3 Provider reachability
+// Seb: check Web3 Provider reachability
+//      https://github.com/ethereum/web3.js/blob/develop/lib/web3/httpprovider.js
 if (!web3.isConnected()) {
   throw "Web3 Provider Host " + host + " or Port " + port + " unreachable!";
 }
